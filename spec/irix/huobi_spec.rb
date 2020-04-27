4d28dbb3e31f8635b953f6fedf50f70f7cb35cb8
Maksym Naichuk <mnaichuk@heliostech.fr>
2020-04-27 22:13:18 +0300

    Update Houbi upstream with public orderbook

--- spec/irix/huobi_spec.rb
+++ spec/irix/huobi_spec.rb
@@ -28,4 +28,39 @@ RSpec.describe Irix::Huobi do
     expect(huobi).to receive(:notify_public_trade).with(trade)
     huobi.detect_trade(msg)
   end
+
+  context "OrderBook proxy" do
+    let(:order_msg) { [[170.16, 28.67834216]] }
+
+    it do
+      huobi.snap = { 'asks' => [], 'bids' => [] }
+      huobi.increment_count = 0
+      huobi.snapshot_time = Time.now
+      expect(huobi).to receive(:fill_increment)
+      huobi.detect_order(order_msg)
+    end
+
+    it do
+      huobi.snap = { 'asks' => [], 'bids' => [] }
+      huobi.increment_count = 101
+      huobi.snapshot_time = Time.now - 60
+      expect(huobi).to receive(:publish_snapshot)
+      expect(huobi).to receive(:fill_increment)
+      huobi.detect_order(order_msg)
+    end
+
+    context '#fill_side' do
+      let(:inc) { {"prevSeqNum"=>100120546808, "seqNum"=>100120546809, "bids"=>[[1201.0, 1.0]], "asks"=>[[1203.0, 1.0]]} }
+
+      it do
+        huobi.asks = []
+        huobi.bids = []
+        huobi.snap = { 'asks' => [], 'bids' => [] }
+        huobi.fill_side(inc, "asks")
+        huobi.fill_side(inc, "bids")
+        expect(huobi.asks).to eq([["1203.0", "1.0"]])
+        expect(huobi.bids).to eq([["1201.0", "1.0"]])
+      end
+    end
+  end
 end
